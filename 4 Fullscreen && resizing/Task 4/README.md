720d0c133c0943ba20cc53919e739c6a18c80582
fernoe1 <krutoytemirlan2007@mail.ru>
2026-02-19 19:43:04 +0600

    feat(fullscreen): task four

---
+++ 4 Fullscreen && resizing/Task 4/README.md
@@ -0,0 +1,2 @@
+Continuation from the previous task. Create window event listener for double click that makes the user enter and exit fullscreen. RECOMMENDATION: Instead of continuing from the previous tasks, rewrite the whole code again, to memorize the syntax.
+
---
+++ 4 Fullscreen && resizing/Task 4/solution.js
@@ -0,0 +1,88 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', (event) => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+});
+
+window.addEventListener('dblclick', (event) => {
+    const fullscreenElement = document.fullscreenElement || document.webkitFullScreenElement;
+
+    if (!fullscreenElement) {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
+        } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+});
+
+const canvas = document.querySelector('canvas.webgl');
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+scene.add(camera);
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(cube);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const tick = () => {
+    controls.update();
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+}
+
+renderer.render(scene, camera);
+
+tick();
+
+/** CSS
+*
+{
+    margin: 0;
+    padding: 0;
+}
+
+html,
+body
+{
+    overflow: hidden;
+}
+
+.webgl
+{
+    position: fixed;
+    top: 0;
+    left: 0;
+    outline: none;
+}
+ */
