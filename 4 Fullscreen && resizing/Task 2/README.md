5c9049dc60dac6c451c211fa10664b19f1b99b78
fernoe1 <krutoytemirlan2007@mail.ru>
2026-02-19 13:37:26 +0600

    feat(fullscreen): task two

---
+++ 4 Fullscreen && resizing/Task 2/README.md
@@ -0,0 +1,4 @@
+Continuation from the previous task.
+Listen to the resize event and handle resizing. (Hint: Use 'resize' window event listener, camera.aspect, camera.updateProjectionMatrix(), renderer.setSize) 
+RECOMMENDATION: Instead of continuing from the previous tasks, rewrite the whole code again, to memorize the syntax.
+
---
+++ 4 Fullscreen && resizing/Task 2/solution.js
@@ -0,0 +1,71 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', (event) => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+});
+
+const canvas = document.querySelector('canvas.webgl');
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+scene.add(camera);
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(cube);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const tick = () => {
+    controls.update();
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+}
+
+renderer.render(scene, camera);
+
+tick();
+
+/** CSS
+
+*
+{
+    margin: 0;
+    padding: 0;
+}
+
+html,
+body
+{
+    overflow: hidden;
+}
+
+.webgl 
+{
+    position: fixed;
+    top: 0;
+    left: 0;
+}
+
+*/
