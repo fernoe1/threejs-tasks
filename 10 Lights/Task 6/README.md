071c5209c6e9bab3cc0681ce233c01465432081e
fernoe1 <krutoytemirlan2007@mail.ru>
2026-03-01 03:20:32 +0600

    feat(lights): task six

---
+++ 10 Lights/Task 6/README.md
@@ -0,0 +1,30 @@
+Create universal MeshStandardMaterial.
+
+
+
+Use that material to create cube, sphere, torus, and plane meshes.
+
+
+
+Animate cube, sphere, torus to continuously rotate on both x and y axises.
+
+
+
+Create spotlight.
+
+
+
+Using lil-gui, add intensity, distance, angle, penumbra, decay sliders to spotlight.
+
+
+
+You must have CSS that makes your canvas take whole screen.
+
+
+
+You must have resize event listener that listens to resizes.
+
+
+
+You must have 'h' keypress event listener that hides/shows the GUI.
+
---
+++ 10 Lights/Task 6/solution.js
@@ -0,0 +1,142 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import GUI from 'lil-gui';
+const gui = new GUI({
+    title: 'debug',
+    closeFolders: true
+});
+
+addEventListener('keypress', (event) => {
+    if (event.key === 'h') gui.show(gui._hidden);
+});
+
+const sizes = {
+    width: innerWidth,
+    height: innerHeight
+};
+
+addEventListener('resize', () => {
+    sizes.width = innerWidth;
+    sizes.height = innerHeight;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(2, devicePixelRatio));
+});
+
+const canvas = document.querySelector('canvas.webgl');
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(2, devicePixelRatio));
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const material = new THREE.MeshStandardMaterial();
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+);
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+);
+sphere.position.x = -1.5;
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+);
+torus.position.x = 1.5;
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+);
+plane.rotation.x = -Math.PI * 0.5;
+plane.position.y = -0.65;
+
+scene.add(cube, sphere, torus, plane);
+
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1);
+scene.add(spotLight);
+
+gui
+    .add(spotLight, 'intensity')
+    .min(0)
+    .max(10)
+    .step(0.01);
+
+gui
+    .add(spotLight, 'distance')
+    .min(0)
+    .max(10)
+    .step(0.01);
+
+gui
+    .add(spotLight, 'angle')
+    .min(0)
+    .max(Math.PI * 2)
+    .step(0.01);
+
+gui
+    .add(spotLight, 'penumbra')
+    .min(0)
+    .max(1)
+    .step(0.01);
+
+gui
+    .add(spotLight, 'decay')
+    .min(0)
+    .max(1)
+    .step(0.01);
+
+const meshes = [sphere, cube, torus]
+const clock = new THREE.Clock();
+const tick = (timestamp) => {
+    const elapsedTime = clock.getElapsedTime();
+
+    meshes.forEach((mesh) => {
+        mesh.rotation.y = 0.1 * elapsedTime;
+        mesh.rotation.x = -0.15 * elapsedTime;
+    });
+
+    controls.update();
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(tick);
+};
+
+tick();
+
+/** CSS
+*
+{
+    margin: 0;
+    padding: 0;
+}
+
+html,
+body
+{
+    overflow: hidden;
+}
+
+.webgl
+{
+    position: fixed;
+    top: 0;
+    left: 0;
+    outline: none;
+}
+*/
